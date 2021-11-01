b7a039eeda7fca18c1f807024ea33374b28bddf4
muhdharun <harunabdul@yahoo.com.sg>
2021-11-01 20:10:01 +0800

    Finished Pub\sub class, sync peer on startup

--- python-blockchain/README.md
+++ python-blockchain/README.md
@@ -21,3 +21,12 @@ Make sure to activate the venv
 ```
 python -m backend.app
 ```
+
+**Run a peer instance**
+
+Make sure to activate venv.
+
+```
+
+export PEER=True && python3 -m backend.app
+```
--- python-blockchain/backend/app/__init__.py
+++ python-blockchain/backend/app/__init__.py
@@ -1,9 +1,14 @@
+import os
+import random
+import requests
 from flask import Flask, jsonify
 
 from backend.blockchain.blockchain import Blockchain
+from backend.pubsub import PubSub
 
 app = Flask(__name__)
 blockchain = Blockchain()
+pubsub = PubSub(blockchain)
 
 @app.route('/')
 def default():
@@ -20,7 +25,27 @@ def route_blockchain_mine():
 
     blockchain.add_block(transaction_data)
 
-    return jsonify(blockchain.chain[-1].to_json())
+    block = blockchain.chain[-1]
+    pubsub.broadcast_block(block)
 
-app.run()
+    return jsonify(block.to_json())
+
+ROOT_PORT = 5000
+PORT = ROOT_PORT
+
+if os.environ.get('PEER') == 'True':
+    PORT = random.randint(5001, 6000)
+
+    result = requests.get(f'http://localhost:{ROOT_PORT}/blockchain')
+    print(f'result.json():{result.json()}')
+
+    result_blockchain = Blockchain.from_json(result.json())
+
+    try:
+        blockchain.replace_chain(result_blockchain.chain)
+        print('\n -- Successfully sync the local chain')
+    except Exception as e:
+        print(f'\n -- Error sync: {e}')
+
+app.run(port=PORT)
 
