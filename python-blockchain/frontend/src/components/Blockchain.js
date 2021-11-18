4bfcde3b1ccd633f9f416cee184c297087db8cff
muhdharun <harunabdul@yahoo.com.sg>
2021-11-18 21:24:23 +0800

    Completed pagination for main page

--- python-blockchain/frontend/src/components/Blockchain.js
+++ python-blockchain/frontend/src/components/Blockchain.js
@@ -1,15 +1,36 @@
 import React, {useState,useEffect} from "react";
-import {API_BASE_URL} from '../config'
+import {API_BASE_URL} from '../config';
+import {Button} from 'react-bootstrap';
 import Block from './Block';
+
+const PAGE_RANGE = 3;
 
 function Blockchain() {
   const [blockchain, setBlockchain] = useState([])
+  const [blockchainLength, setBlockchainLength] = useState(0);
+
+  const fetchBlockchainPage =({start,end}) => {
+    fetch(`${API_BASE_URL}/blockchain/range?start=${start}&end=${end}`)
+      .then(response => response.json())
+      .then(json => setBlockchain(json));
+  }
 
   useEffect(() => {
+    fetchBlockchainPage({start: 0, end: PAGE_RANGE});
+
     fetch(`${API_BASE_URL}/blockchain`)
       .then(response => response.json())
       .then(json => setBlockchain(json));
+    
+    fetch(`${API_BASE_URL}/blockchain/length`)
+      .then(response => response.json())
+      .then(json => setBlockchainLength(json));
   }, []);
+
+  const buttonNumbers = []
+  for (let i=0; i<blockchainLength/PAGE_RANGE; i++){
+    buttonNumbers.push(i);
+  }
 
   return (
     <div className="Blockchain">
@@ -17,6 +38,22 @@ function Blockchain() {
       <div>
         {
           blockchain.map(block => <Block key={block.hash} block={block}/>)
+        }
+      </div>
+      <div>
+        {
+          buttonNumbers.map(number => {
+            const start = number * PAGE_RANGE;
+            const end = (number+1) * PAGE_RANGE;
+
+            return (
+              <span key={number} onClick={() => fetchBlockchainPage({start,end})}>
+                <Button size="sm" variant="danger">
+                  {number+1}
+                </Button>{' '}
+              </span>
+            )
+          })
         }
       </div>
     </div>
